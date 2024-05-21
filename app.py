7dcd8a843cf2ebd8d3527d1221ef4415c9f32736
Kenneth Lai <ken_sh_lai@manulife.com>
2024-05-21 11:23:32 +0800

    added exporter

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-# These are the necessary import declarations
-from opentelemetry import trace
-from opentelemetry import metrics
-
-from random import randint
-from flask import Flask, request
+from flask import Flask
 import logging
+
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader, ConsoleMetricExporter
+
+# Service name is required for most backends,
+# and although it's not necessary for console export,
+# it's good to set service name anyways.
+resource = Resource(attributes={
+    SERVICE_NAME: "your-service-name"
+})
+
+traceProvider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+traceProvider.add_span_processor(processor)
+trace.set_tracer_provider(traceProvider)
+
+reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meterProvider)
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 @app.route("/")
-def selcome():
+def welcome():
     return "Welcome to Opentelemetry-sample"
