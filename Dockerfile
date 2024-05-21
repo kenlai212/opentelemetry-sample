aa0671f91caf760a333f52c99cc5371f16d015b5
Kenneth Lai <ken_sh_lai@manulife.com>
2024-05-21 12:38:00 +0800

    fixed Dockerdile

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN opentelemetry-bootstrap -a install
 
 COPY . .
 
-CMD export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true;opentelemetry-instrument --traces_exporter console --metrics_exporter console --logs_exporter console --service_name dice-server;python3 -m flask run --host=0.0.0.0
+CMD flask run
--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import logging
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
 from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader, ConsoleMetricExporter
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 
 # Service name is required for most backends,
 # and although it's not necessary for console export,
@@ -19,13 +21,15 @@ resource = Resource(attributes={
 })
 
 traceProvider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="localhost:4317/v1/traces"))
 traceProvider.add_span_processor(processor)
 trace.set_tracer_provider(traceProvider)
 
+'''
 reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
 meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
 metrics.set_meter_provider(meterProvider)
+'''
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
